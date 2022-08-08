1ee7c834d2b7c68fe77049dd6e4c6ab5bc4551ea
stuartmarshall-nz <108042140+stuartmarshall-nz@users.noreply.github.com>
2022-08-09 00:30:26 +1200

    Minor edit to unite the three existing classes together and check record.

--- DataCollector/src/nz/net/themyscira/MainClass.java
+++ DataCollector/src/nz/net/themyscira/MainClass.java
@@ -1,9 +1,18 @@
 package nz.net.themyscira;
 
+import nz.net.themyscira.ui.*;
+import nz.net.themyscira.datamodel.*;
+
 public class MainClass {
 
 	public static void main(String[] args) {
 		System.out.println(args.length + " arguments received");
-		}
+		
+		MainWindow win = new MainWindow();
+		win.show();
+		
+		ExerciseRecord rec = new ExerciseRecord();
+		System.out.println("ID of new record is: " + rec.getRecordID());
+	}
 	
 }
