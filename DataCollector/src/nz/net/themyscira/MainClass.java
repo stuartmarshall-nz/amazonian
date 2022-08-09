4ad499151ad3314c069934f9813e7ddd5f0a8da7
stuartmarshall-nz <108042140+stuartmarshall-nz@users.noreply.github.com>
2022-08-09 14:24:59 +1200

    Switched to JavaFX call method to invoke start() on MainWindow class.

--- DataCollector/src/nz/net/themyscira/MainClass.java
+++ DataCollector/src/nz/net/themyscira/MainClass.java
@@ -1,18 +1,23 @@
 package nz.net.themyscira;
 
 import nz.net.themyscira.ui.*;
+
 import nz.net.themyscira.datamodel.*;
+import javafx.application.Application;
+
 
 public class MainClass {
 
 	public static void main(String[] args) {
 		System.out.println(args.length + " arguments received");
 		
-		MainWindow win = new MainWindow();
-		win.show();
+//		MainWindow win = new MainWindow();
 		
 		ExerciseRecord rec = new ExerciseRecord();
 		System.out.println("ID of new record is: " + rec.getRecordID());
+		Application.launch(MainWindow.class, args);
 	}
+	
+	
 	
 }
