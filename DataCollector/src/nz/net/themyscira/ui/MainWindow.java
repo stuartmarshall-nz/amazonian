7497babedd83a23dafafd876a06be8df15a14723
stuartmarshall-nz <108042140+stuartmarshall-nz@users.noreply.github.com>
2022-08-09 14:24:28 +1200

    Added placeholder JavaFX start() method.

--- DataCollector/src/nz/net/themyscira/ui/MainWindow.java
+++ DataCollector/src/nz/net/themyscira/ui/MainWindow.java
@@ -1,8 +1,21 @@
 package nz.net.themyscira.ui;
 
-public class MainWindow {
+import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
 
-	public void show() {
+import javafx.application.*;
+
+public class MainWindow extends Application {
+
+	public void start(Stage stage) {
+		System.out.println("here");
+		Text message = new Text(75,100,"Data Collector Utility");
+		Pane root = new Pane(message);
+		Scene scene = new Scene(root);
+		stage.setScene(scene);
+		stage.show();
 	}
 	
 }
