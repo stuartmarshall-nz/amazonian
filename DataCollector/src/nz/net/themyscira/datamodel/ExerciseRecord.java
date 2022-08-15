61ec26638d7e11fe89fcb78ab9aa2c0efd742db1
stuartmarshall-nz <108042140+stuartmarshall-nz@users.noreply.github.com>
2022-08-16 10:55:00 +1200

    Added String to ExerciseRecord.java

--- DataCollector/src/nz/net/themyscira/datamodel/ExerciseRecord.java
+++ DataCollector/src/nz/net/themyscira/datamodel/ExerciseRecord.java
@@ -4,9 +4,11 @@ public class ExerciseRecord {
 
 	private static int nextRecordID = 1;
 	private int recordID;
+	private String data;
 	
 	public ExerciseRecord() {
 		this.recordID = nextRecordID++;
+		this.data = "placeholder data";
 	}
 	
 	public int getRecordID() {
@@ -15,6 +17,10 @@ public class ExerciseRecord {
 	
 	public static int numRecords() {
 		return nextRecordID - 1;
+	}
+	
+	public void print() {
+		System.out.println("data: " + this.data);
 	}
 	
 }
