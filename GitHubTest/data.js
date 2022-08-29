35e5e1e1df15831ad8c9fbee788d4193cd251bf1
Stuart Marshall <stuartrichardmarshall@gmail.com>
2022-08-29 18:23:39 +1200

    adding new examples of copilot

--- GitHubTest/data.js
+++ GitHubTest/data.js
@@ -6,6 +6,12 @@ function calculateDaysBetweenDates(begin, end) {
     return Math.round(Math.abs((firstDate.getTime() - secondDate.getTime())/(oneDay)));
 }
 
+function loopseries(start, end, callback) {
+    for (var i = start; i <= end; i++) {
+        callback(i);
+    }
+}
+
 function calculateMax(num1, num2) {
     return Math.max(num1, num2);
 }
